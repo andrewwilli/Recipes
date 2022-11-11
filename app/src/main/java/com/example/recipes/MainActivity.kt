733528632b6662cf098640aa2d90f0f6f88415ee
Andrew Willi <andrew.willi@ost.ch>
2022-11-11 16:58:22 +0100

    button to recipe

--- app/src/main/java/com/example/recipes/MainActivity.kt
+++ app/src/main/java/com/example/recipes/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.example.recipes
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
+
+
 class MainActivity : AppCompatActivity() {
 
     lateinit var toggle : ActionBarDrawerToggle
@@ -15,7 +19,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        // deleteBaby
+        var btn = findViewById<Button>(R.id.buttonToRecipe)
+        btn.setOnClickListener {
+            val intent = Intent(this, RecipeActivity::class.java)
+            startActivity(intent)
+        }// deleteBaby
         val drawerLayout : DrawerLayout = findViewById(R.id.drawerLayout)
         val navView : NavigationView = findViewById(R.id.nav_view)
 
